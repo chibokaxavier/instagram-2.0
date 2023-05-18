2834f578710aaaf7e5408c42d5d40a0009f820b1
manlikexavy <chibokaxavier@gmail.com>
2023-05-18 08:02:43 -0700

    Header completed. next up is the Feed

---
+++ components/Header.js
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import React from "react";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import {HomeIcon} from "@heroicons/react/solid";
+
+const Header = () => {
+  return (
+    <div className="shadow-sm border-b bg-white sticky z-50 top-0 ">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+        {/* left */}
+        <div className="relative w-24 h-24  items-center hidden lg:inline-grid cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            alt=""
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+        <div className="relative w-10 lg:hidden items-center  flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            alt=""
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+
+        {/* middle */}
+        <div className="max-w-xs">
+        <div className="relative mt-1 p-3 rounded-md ">
+          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+            <SearchIcon className="h-5 w-5 text-gray-500  " />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="pl-10 block bg-gray-50 w-full sm:text-sm border-gray-300 focus:ring-black  rounded-md focus:border-black"
+          />
+        </div>
+        </div>
+       
+        {/* right */}
+        <div className="flex justify-end items-center space-x-4 ">
+            <HomeIcon className="navBtn"/>
+            <MenuIcon className="h-6 md:hidden cursor-pointer"/>
+<div className="navBtn relative">
+<PaperAirplaneIcon className="rotate-45 navBtn"/>
+<div className="absolute -right-2 -top-1 text-xs h-5 w-5 bg-red-500 flex items-center justify-center rounded-full animate-pulse text-white">
+    3
+</div>
+</div>
+           
+            <PlusCircleIcon className="navBtn"/>
+            <UserGroupIcon className="navBtn"/>
+            <HeartIcon className="navBtn"/>
+            <div className="h-10 rounded-full">
+            <img src="https://cdn.pixabay.com/photo/2023/05/01/06/19/penguin-7962192__340.jpg" alt="" className="rounded-full w-10 h-full cursor-pointer"/>
+            </div>
+
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
