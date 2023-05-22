2107075e9b94105adea7ada09089f06688295620
manlikexavy <chibokaxavier@gmail.com>
2023-05-22 06:51:05 -0700

    UPLOADING IMAGE AND CAPTION FUNCTIONALITY ALL WORKING NOW, UP NEXT IS TO DISPLAY IT ON THE FEED

--- components/Modal.js
+++ components/Modal.js
@@ -3,11 +3,40 @@ import React, { Fragment, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { Dialog, Transition } from "@headlessui/react";
 import { CameraIcon } from "@heroicons/react/outline";
+import {db,storage} from "../firebase"
+import { collection, addDoc, serverTimestamp, updateDoc, doc } from "firebase/firestore"; 
+import { useSession } from "next-auth/react";
+import {ref,getDownloadURL,uploadString} from "@firebase/storage"
 
 const Modal = () => {
+    const {data:session} = useSession()
   const [open, setOpen] = useRecoilState(modalState);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
   const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+
+  const uploadPost = async () => {
+    if (loading) return;
+    setLoading(true);
+    const docRef = await addDoc(collection(db,"posts"),{
+        username: session?.user?.username,
+        caption:captionRef.current.value,
+        profileImg:session?.user?.image,
+        timestamp:serverTimestamp()
+    })
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`)
+await uploadString(imageRef,selectedFile, "data_url").then(async snapshot=>{
+    const downloadUrl = await getDownloadURL(imageRef);
+    await updateDoc(doc(db,'posts',docRef.id),{
+        image:downloadUrl
+    })
+    setOpen(false)
+    setLoading(false)
+    setSelectedFile(null)
+})
+      };
 
   const addImageToPost = (e) => {
     const reader = new FileReader();
@@ -58,7 +87,12 @@ const Modal = () => {
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
                 {selectedFile ? (
-                  <img src={selectedFile} onClick={()=>setSelectedFile(null)} alt="" className="cursor-pointer w-full object-contain h-[200px]" />
+                  <img
+                    src={selectedFile}
+                    onClick={() => setSelectedFile(null)}
+                    alt=""
+                    className="cursor-pointer w-full object-contain h-[200px]"
+                  />
                 ) : (
                   <div
                     onClick={() => filePickerRef.current.click()}
@@ -92,6 +126,7 @@ const Modal = () => {
                         type="text"
                         className="border-none focus:ring-0 w-full text-center "
                         placeholder="Please Enter a caption"
+                        ref={captionRef}
                       />
                     </div>
                   </div>
@@ -99,9 +134,10 @@ const Modal = () => {
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    className="inline-flex justify-center w-full rounded-md border border-tranparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300  disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    disabled={!selectedFile}
+                    className="inline-flex justify-center w-full rounded-md border border-tranparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300  disabled:cursor-not-allowed hover:disabled:bg-gray-300" onClick={uploadPost}
                   >
-                    Upload Post
+                   {loading?('Uploading'):('Loading')}
                   </button>
                 </div>
               </div>
--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 // Import the functions you need from the SDKs you need
+import { getFirestore } from "@firebase/firestore";
+import { getStorage } from "@firebase/storage";
 import { initializeApp, getApp, getApps } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
