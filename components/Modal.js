c5e3a32a1f3832f48b3e9a9cf1a12636bb48e065
manlikexavy <chibokaxavier@gmail.com>
2023-05-22 07:09:00 -0700

    SPINNER COMPONENT CREATED

--- components/Modal.js
+++ components/Modal.js
@@ -3,40 +3,49 @@ import React, { Fragment, useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import { Dialog, Transition } from "@headlessui/react";
 import { CameraIcon } from "@heroicons/react/outline";
-import {db,storage} from "../firebase"
-import { collection, addDoc, serverTimestamp, updateDoc, doc } from "firebase/firestore"; 
+import { db, storage } from "../firebase";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import {ref,getDownloadURL,uploadString} from "@firebase/storage"
+import { ref, getDownloadURL, uploadString } from "@firebase/storage";
 
 const Modal = () => {
-    const {data:session} = useSession()
+  const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
   const [selectedFile, setSelectedFile] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const filePickerRef = useRef(null);
   const captionRef = useRef(null);
+  const isDisabled= loading || !selectedFile
 
   const uploadPost = async () => {
     if (loading) return;
     setLoading(true);
-    const docRef = await addDoc(collection(db,"posts"),{
-        username: session?.user?.username,
-        caption:captionRef.current.value,
-        profileImg:session?.user?.image,
-        timestamp:serverTimestamp()
-    })
+    const docRef = await addDoc(collection(db, "posts"), {
+      username: session?.user?.username,
+      caption: captionRef.current.value,
+      profileImg: session?.user?.image,
+      timestamp: serverTimestamp(),
+    });
 
-    const imageRef = ref(storage, `posts/${docRef.id}/image`)
-await uploadString(imageRef,selectedFile, "data_url").then(async snapshot=>{
-    const downloadUrl = await getDownloadURL(imageRef);
-    await updateDoc(doc(db,'posts',docRef.id),{
-        image:downloadUrl
-    })
-    setOpen(false)
-    setLoading(false)
-    setSelectedFile(null)
-})
-      };
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+    await uploadString(imageRef, selectedFile, "data_url").then(
+      async (snapshot) => {
+        const downloadUrl = await getDownloadURL(imageRef);
+        await updateDoc(doc(db, "posts", docRef.id), {
+          image: downloadUrl,
+        });
+        setOpen(false);
+        setLoading(false);
+        setSelectedFile(null);
+      }
+    );
+  };
 
   const addImageToPost = (e) => {
     const reader = new FileReader();
@@ -134,10 +143,16 @@ await uploadString(imageRef,selectedFile, "data_url").then(async snapshot=>{
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
-                    disabled={!selectedFile}
-                    className="inline-flex justify-center w-full rounded-md border border-tranparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300  disabled:cursor-not-allowed hover:disabled:bg-gray-300" onClick={uploadPost}
+                    disabled={!selectedFile||loading}
+                    
+                    className="inline-flex justify-center w-full rounded-md border border-tranparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300  disabled:cursor-not-allowed hover:disabled:bg-gray-300"
+                    onClick={uploadPost}
                   >
-                   {loading?('Uploading'):('Loading')}
+                    {loading ? (
+                     
+                    ) : (
+                      "Upload Post"
+                    )}
                   </button>
                 </div>
               </div>
